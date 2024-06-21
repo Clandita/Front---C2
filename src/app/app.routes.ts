277b7c27e7a26bf9dbe1b789b31079d5596857f8
Villagol <mar.villablanca2@gmail.com>
2024-06-21 14:30:15 -0400

    Navbar Bonito y enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListadoComponent } from './components/listado/listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },{
+        path:'tareas',
+        component: ListadoComponent
+    }
+];
