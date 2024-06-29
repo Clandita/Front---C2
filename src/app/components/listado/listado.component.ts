77e1b350abce1e6c9528d5b0c88d5c9abdf1db6b
Villagol <mar.villablanca2@gmail.com>
2024-06-29 13:53:20 -0400

    Calendario y detalle

--- src/app/components/listado/listado.component.ts
+++ src/app/components/listado/listado.component.ts
@@ -11,33 +11,58 @@ import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule }
 @Component({
   selector: 'listado',
   standalone: true,
-  imports: [CommonModule,TareaComponent,MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
+  imports: [CommonModule, TareaComponent, MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
   providers: [provideNativeDateAdapter()],
-  
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './listado.component.html',
-  styleUrl: './listado.component.scss'
+  styleUrls: ['./listado.component.scss']
 })
-export class ListadoComponent  implements OnInit {
-  form:FormGroup;
+export class ListadoComponent implements OnInit {
+  form: FormGroup;
 
-  tareas: Tarea[]=[];
+  tareas: Tarea[] = [];
+  filteredTareas: Tarea[] = [];
   readonly range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
-  
-  
 
-  constructor(private tareaService:TareaService,private formBuilder:FormBuilder){
+  constructor(private tareaService: TareaService, private formBuilder: FormBuilder) {}
 
-  }
-  ngOnInit():void{
+  ngOnInit(): void {
     this.tareas = this.tareaService.getTareas();
-    this.form=this.formBuilder.group({
-      
-    })
+    this.filteredTareas = this.tareas;
+    this.form = this.formBuilder.group({});
+
+    this.range.valueChanges.subscribe(() => {
+      this.filterTareasByDate();
+    });
   }
 
+  filterTareasByDate() {
+    const rangeValue = this.range.value;
+    if (rangeValue) {
+      const { start, end } = rangeValue;
+      if (start && end) {
+        this.filteredTareas = this.tareas.filter(tarea => {
+          const tareaFecha = new Date(tarea.fecha);
+          return tareaFecha >= start && tareaFecha <= end;
+        });
+      } else {
+        this.filteredTareas = this.tareas;
+      }
+    } else {
+      this.filteredTareas = this.tareas;
+    }
+  }
 
+  filterTareasByTitle(searchTerm: string) {
+    this.filteredTareas = this.tareas.filter(tarea => {
+      return tarea.titulo.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  }
+
+  trackByFn(index: any, item: any) {
+    return item.id;
+  }
 }
