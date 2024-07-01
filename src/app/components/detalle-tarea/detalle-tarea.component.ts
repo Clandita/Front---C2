353ab17ddd9bb0a122d5bfb0c857b77b3cb4a631
Clandita <111799339+Clandita@users.noreply.github.com>
2024-06-30 20:25:50 -0400

    tamañoss

--- src/app/components/detalle-tarea/detalle-tarea.component.ts
+++ src/app/components/detalle-tarea/detalle-tarea.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Tarea } from '../../models/tarea.model';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { TareaService } from '../../services/tarea.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-detalle-tarea',
   standalone: true,
-  imports: [CommonModule, MatCardModule ,RouterLink, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatCardModule ,RouterLink, MatButtonModule, MatIconModule,],
   templateUrl: './detalle-tarea.component.html',
   styleUrl: './detalle-tarea.component.scss'
 })
@@ -22,6 +22,7 @@ export class DetalleTareaComponent {
     private activatedRouter: ActivatedRoute,
     private router:Router,
     private tareaService:TareaService,
+    private location: Location,
 
   ){
     this.activatedRouter.params.subscribe(
@@ -32,7 +33,7 @@ export class DetalleTareaComponent {
     )
   }
   back() {
-    this.router.navigate(['/tareas']);
+    this.location.back();
   }
 
 
